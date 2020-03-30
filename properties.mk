070d7d089a067606a0c7856c2752da02f16fb0b1
Henrique Pereira <hlcpereira@pixelexperience.org>
2020-03-30 15:31:20 -0300

    Add face unlock cam id property

--- properties.mk
+++ properties.mk
@@ -35,3 +35,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.adb.secure=0 \
     ro.secure=0 \
     ro.debuggable=1
+
+# Face Unlock
+PRODUCT_PROPERTY_OVERRIDES += \
+   ro.face.moto_unlock_service.cam_id=5
