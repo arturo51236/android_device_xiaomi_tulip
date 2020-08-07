d847e705cc5e8e1a8bd85289e9843cec4a9527d1
arturo51236 <rtsanonynousperson@gmail.com>
2020-08-07 04:47:10 +0200

    Bringup Evox...

--- aosp_tulip.mk
+++ aosp_tulip.mk
@@ -19,11 +19,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
-# Inherit some common Lineage stuff
+# Inherit some common Evox stuff
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Inherit from tulip device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
+
 # Some common aosp Properties
 TARGET_INCLUDE_WIFI_EXT := true
 TARGET_GAPPS_ARCH := arm64
@@ -41,4 +42,3 @@ TARGET_VENDOR_PRODUCT_NAME := tulip
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="coral-user 10 QQ3A.200605.001 6392402 release-keys"
-
