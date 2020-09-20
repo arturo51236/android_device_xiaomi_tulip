736da18b05f65c00e164598ab01e56bfec49edc3
arturo51236 <rtsanonynousperson@gmail.com>
2020-09-20 11:29:15 +0200

    Revert "Nuke NoCutOutOverlay"
    This reverts commit a66b73f193bb7a1f53304cb8abb19fd0c578f803.

--- device.mk
+++ device.mk
@@ -79,6 +79,9 @@ PRODUCT_COPY_FILES += \
 DEVICE_PACKAGE_OVERLAYS += \
     $(DEVICE_PATH)/overlay
 
+PRODUCT_PACKAGES += \
+    NoCutoutOverlay
+
 # Ramdisk
 PRODUCT_PACKAGES += \
     init.device.rc
